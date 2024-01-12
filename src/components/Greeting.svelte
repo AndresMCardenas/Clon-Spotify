90686e0b1a79e233290284f47daa8098ef2235ef
Andres M. Cardenas <andrescar18@gmail.com>
2024-01-12 17:08:31 -0500

    se agregan componente de react y svelte

---
+++ src/components/Greeting.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  const currenTime = new Date()
+  const currenHours = currenTime.getHours()
+
+  let greeting = ''
+
+  if (currenHours < 12) {
+    greeting = 'Buenos dias'
+  } else if (currenHours < 18) {
+    greeting = 'Buenas tardes'
+  } else {
+    greeting = 'Buenas noches'
+  }
+</script>
+
+<h1 class="text-3xl font-bold">{greeting}</h1>
